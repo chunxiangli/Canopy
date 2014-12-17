ae7b19919429e08aabf31177d687bb1704ef52bb
czli <julia.lcx@gmail.com>
2014-12-17 14:50:14 +0200

    update the README.md

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Canopy
 ======
 
-Iterative alignment and phylogeny inference tool with PRANK and PAGAN
+Parallelised iterative search of optimal phylogeny and phylogeny-aware sequence alignment
 
 The tool was developed in Loytynoja's group:http://loytynojalab.biocenter.helsinki.fi.
 
-The installation and usage can be found in http://wasabiapp.org/software/canopy/.
+The detailed installation and usage instructions can be found in http://wasabiapp.org/software/canopy.
--- canopy/main.py
+++ canopy/main.py
@@ -1,3 +1,19 @@
+#Canopy: Parallelised iterative search of optimal phylogeny and phylogeny-aware sequence alignment
+#Copyright (C) 2014  Chunxiang LI (julia.lcx@gmail.com)
+#
+##This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+##This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+##You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 from canopy import msg_exit
 if sys.version_info < (2,7) :
